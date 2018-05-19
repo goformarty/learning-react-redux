fff0e6f874d1d71e5bbeb90256253694cc779a18
Marty Stepien <marty.stepien@gmail.com>
2018-05-19 13:18:51 +0100

    udemy tutorial

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+
+const App = function() {
+	 return <div>Hi!</div>
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
